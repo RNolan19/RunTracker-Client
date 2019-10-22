57c1dc77b31f56f1230ac92c6ded798a039bbb4c
RNolan19 <rnolan19@gmail.com>
2019-10-22 12:13:55 -0400

    changed production apiURL + Run History lingo

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<https://tranquil-lake-32578.herokuapp.com>',
+  production: 'https://tranquil-lake-32578.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
